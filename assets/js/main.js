9b488ed7bf939642379ab6781b60fd2cdd28eaba
Rhytah <ritanamono@gmail.com>
2018-09-17 17:34:30 +0300

    close list item onclick [Delivers #160453702]

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,36 @@ function sform(a) {
         document.getElementById("SignUp").style.display = "block";
 }
 
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+    div.txt="delete"
+  }
+}
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
@@ -50,10 +80,3 @@ function addlist(a) {
       document.getElementById("todoadd2").style.display = "block";
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
