c8dbd8aee191e78d47bfc2f673bf4369b07d7d71
Rhytah <ritanamono@gmail.com>
2018-09-17 20:02:06 +0300

    hide login page

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,15 @@ e.style.display = 'block';
 else
 e.style.display = 'none';
 }
+//hide login
+function hide(id) {
+  var d = document.getElementById(id);
+  if(d.style.display == 'block')
+  d.style.display ='none';
+  else
+  d.style.display='block';
+}
+
 //signup message 
 function singupAlert(){
     alert("Sign up successful");
