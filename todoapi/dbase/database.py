393358254cf26f5bf4d728f8777e44436321bfe7
Rhytah <ritanamono@gmail.com>
2018-09-28 20:58:24 +0300

    update

--- todoapi/dbase/database.py
+++ todoapi/dbase/database.py
@@ -14,6 +14,45 @@ class DatabaseConnection(object):
             print(e)
             print("Connection failed")
 
+    def create_tables(self):
+        cmds=(
+            """
+            CREATE TABLE IF NOT EXIST user(
+                PRIMARY KEY user_id SERIAL,
+                firt_name VARCHAR (30), 
+                last_name VARCHAR (30),
+                age INT,
+                email VARCHAR (30),
+                password VARCHAR (10)
+                created_at timestamp DEFAULT CURRENT_TIMESTAMP)
+            );
+            """
+            """
+            CREATE TABLE IF NOT EXIST categories(
+                PRIMARY KEY category_id SERIAL ,
+                category_name VARCHAR(20),
+                title VARCHAR (20),
+                description VARCHAR (30),
+                created_at TIMESTAMP
+            );
+            """
+            """
+            CREATE TABLE IF NOT EXIST tasks(
+                PRIMARY KEY task_id SERIAL,
+                user_id FOREIGN KEY,
+                title VARCHAR (20),
+                description VARCHAR (30),
+                created_at timestamp DEFAULT CURRENT_TIMESTAMP
+            );
+            """,
+        )
+        for cmd in cmds:
+            self.cursor.execute(cmd)
+            
+
+
+         
+
 
 
 
