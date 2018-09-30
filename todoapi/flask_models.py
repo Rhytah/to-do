69006ee968decadf0eba971ac41a88054e2c6e08
Rhytah <ritanamono@gmail.com>
2018-09-30 22:49:31 +0300

    add task closs to models

---
+++ todoapi/flask_models.py
@@ -0,0 +1,20 @@
+class User:
+    def __init__(self, name,username,email,password):
+        self.name=name
+        self.username=username
+        self.emal=email
+        self.password=password
+
+user={}
+accounts=[]
+
+class Task:
+    def __init__(self,task_id,title,details):
+    
+        self.task_id=task_id
+        self.title=title
+        self.details=details
+
+todo=[]
+tasks={}
+        
